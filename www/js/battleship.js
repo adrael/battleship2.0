5750efb648a6073ab80067581fc40cc9da8072dc
Raphael MARQUES <work.rmarques@gmail.com>
2016-09-24 13:44:10 +0200

    Fixing console override error in development mode

--- www/js/battleship.js
+++ www/js/battleship.js
@@ -1,39 +1,11 @@
-(function () {
+if(/*__debugDisabled__*/ false /*__debugDisabled__*/) {
 
-    'use strict';
+    // CONSOLE OVERRIDE
+    var console = {};
+    console.log = function () { /* noop */ };
+    console.info = function () { /* noop */ };
+    console.error = function () { /* noop */ };
+    console.debug = function () { /* noop */ };
+    window.console = console;
 
-    /**********************************************************************************/
-    /*                                                                                */
-    /*                                 CONFIGURATION                                  */
-    /*                                                                                */
-    /**********************************************************************************/
-
-    if(/*__debugDisabled__*/ false /*__debugDisabled__*/) {
-
-        // CONSOLE OVERRIDE
-        var console = {};
-        console.log = function () { /* noop */ };
-        console.info = function () { /* noop */ };
-        console.error = function () { /* noop */ };
-        console.debug = function () { /* noop */ };
-        window.console = console;
-
-    }
-
-    console.debug('BATTLESHIP 2.0 - Loading game...');
-
-    /**********************************************************************************/
-    /*                                                                                */
-    /*                                     SETUP                                      */
-    /*                                                                                */
-    /**********************************************************************************/
-
-    document.addEventListener('DOMContentLoaded', function() {
-
-        bs.display.drawGrid();
-        bs.display.drawIndexes();
-        bs.display.drawRandomShips();
-
-    });
-
-})();
+}
